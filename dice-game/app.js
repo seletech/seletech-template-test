6e445fb57566d379507a12eb991b2b7d721add93
seletech <seletechtrainer@gmail.com>
2022-04-28 20:24:59 +0530

    dice game

--- dice-game/app.js
+++ dice-game/app.js
@@ -8,3 +8,85 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores,roundScores,activePlayer,gamePlaying;
+const current0 = document.getElementById('current-0');
+const current1 = document.getElementById('current-1');
+const score0 = document.getElementById('score-0');
+const score1 = document.getElementById('score-1');
+const player0 = document.querySelector('.player-0-panel');
+const player1 = document.querySelector('.player-1-panel');
+const name0 = document.querySelector('name-0');
+const name1 = document.querySelector('name-1');
+const diceDOM = document.querySelector('.dice');
+gamePlaying= true;
+init();
+
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+
+   if(gamePlaying){
+      let dice = Math.floor(Math.random()*6)+1;
+
+    
+      diceDOM.style.display='block';
+      diceDOM.src='dice-'+dice+'.png';
+     
+      if(dice !== 1){
+          
+       roundScores += dice;
+       document.querySelector('#current-'+activePlayer).textContent = roundScores;
+ 
+      }
+      else{
+       nextPlayer();
+          
+      }
+   }
+    
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click',function(){
+   if(gamePlaying){
+   scores[activePlayer] += roundScores;
+   document.querySelector('#score-'+activePlayer).textContent =scores[activePlayer];
+   
+
+   if(scores[activePlayer]>=20){
+      document.querySelector('#name-'+activePlayer).textContent = 'Winner!';
+      document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+      document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+      diceDOM.style.display='none';
+      gamePlaying = false;
+   }
+   else{
+      nextPlayer();
+   }
+  
+}   
+});
+
+document.querySelector('.btn-new').addEventListener('click',init);
+
+function nextPlayer(){
+   activePlayer === 0 ? activePlayer=1 :activePlayer=0;
+   roundScores=0;
+   current0.textContent = current1.textContent =0;
+   player0.classList.toggle('active');
+   player1.classList.toggle('active');
+ 
+}
+
+function init(){
+   scores = [0,0];
+   roundScores = 0;
+   activePlayer=0;
+   current0.textContent = current1.textContent = score0.textContent = score1.textContent =0;
+   diceDOM.style.display='none';
+   player0.classList.remove('winner');
+   player1.classList.remove('winner');
+   player0.classList.remove('active');
+   player1.classList.remove('active');
+   player0.classList.add('active');
+   }
