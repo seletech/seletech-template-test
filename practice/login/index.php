1066cd736b01c95a6f551aa83ac260b69dfcfaf4
seletech <seletechtrainer@gmail.com>
2022-05-01 21:22:03 +0530

    php test

---
+++ practice/login/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+
+$title = "Login site";
+$name ="bhuvan";
+//regular array
+$numberList = array(0,1,2,"test","<h1> h1 </h1>");
+//assosiative array
+$numList = ["k1"=>"v1","k2"=>"v2"];
+echo "testing";
+?>
+
+</body>
+</html>
